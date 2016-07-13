6d5e07165fa469b1f99dd93b334d50a9e67572d4
Rebekah Milford <rmilford86@gmail.com>
2016-07-13 10:26:49 -0400

    first commit of inventory

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 "use strict";
 
 (function(){
+  angular
+  .module("inventory", [])
+  .controller("inventory_controller", InventoryController)
 
+  function InventoryController(){
+     var vm = this;
+     vm.data = data;
+   }
+   vm.reveal = function (name) {
+                var pos = vm.data.map(function(e) { return e.name; }).indexOf(name);
+                vm.pic = vm.data[pos].img;
+            }
 })();
